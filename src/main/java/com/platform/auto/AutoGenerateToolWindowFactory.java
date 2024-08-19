192a161d11c1417cad0a1155566a698210869210
yangpu <yangpu@ksmglobal.tech>
2024-08-19 16:22:05 +0800

    12

--- src/main/java/com/platform/auto/AutoGenerateToolWindowFactory.java
+++ src/main/java/com/platform/auto/AutoGenerateToolWindowFactory.java
@@ -2,14 +2,11 @@ package com.platform.auto;
 
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
 
 final class AutoGenerateToolWindowFactory implements ToolWindowFactory, DumbAware {
 
@@ -18,11 +15,6 @@ final class AutoGenerateToolWindowFactory implements ToolWindowFactory, DumbAwar
         AutoGenerateToolWindowContent toolWindowContent = new AutoGenerateToolWindowContent(toolWindow, project);
         Content content = ContentFactory.getInstance().createContent(toolWindowContent.getParentPanel(), "", false);
         toolWindow.getContentManager().addContent(content);
-    }
-
-    @Override
-    public Icon getIcon() {
-        return IconLoader.getIcon("/toolWindow/auto-generate.svg");
     }
 
 }
