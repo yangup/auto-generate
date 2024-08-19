56e6996601c06780d3b39dcead860c6e0744789b
yangpu <yangpu@ksmglobal.tech>
2024-08-19 10:39:02 +0800

    2 1

--- src/main/java/com/platform/auto/AutoGenerateToolWindowContent.java
+++ src/main/java/com/platform/auto/AutoGenerateToolWindowContent.java
@@ -2,10 +2,8 @@ package com.platform.auto;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
-import com.jfoenix.controls.JFXButton;
 import com.platform.auto.config.Config;
 import com.platform.auto.config.DbEntity;
-import com.platform.auto.config.LocalEntity;
 import com.platform.auto.jdbc.Connection;
 import com.platform.auto.sys.log.AutoLogger;
 import com.platform.auto.sys.log.Logger;
@@ -14,9 +12,7 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.*;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public class AutoGenerateToolWindowContent {
@@ -51,6 +47,10 @@ public class AutoGenerateToolWindowContent {
     private void createContentPanel() throws Exception {
         contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS)); // 垂直排列
         contentPanel.add(tableNameFilter);
+
+    }
+
+    private void addTableName() {
         for (DbEntity dbEntity : Config.getLocal().dbInfoList) {
             for (String tableName : dbEntity.tableNameList) {
                 JButton button = new JButton(String.format("%s.%s", dbEntity.dbName, tableName));
@@ -59,24 +59,6 @@ public class AutoGenerateToolWindowContent {
             }
         }
     }
-
-//    @NotNull
-//    private JPanel createControlsPanel(ToolWindow toolWindow) {
-//        JPanel controlsPanel = new JPanel();
-//        controlsPanel.add(runButton);
-//        runButton.setText(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-//        runButton.addActionListener(e -> {
-//            try {
-//                Thread.currentThread().setContextClassLoader(AutoGenerateToolWindowFactory.class.getClassLoader());
-//                Application.start();
-//            } catch (Exception ex) {
-//                logger.info(AutoLogger.getExceptionInfo(ex));
-//            }
-//        });
-//        controlsPanel.add(cancelButton);
-//        cancelButton.addActionListener(e -> toolWindow.hide(null));
-//        return controlsPanel;
-//    }
 
     public void initStartAsync(Project project) {
         new Thread(this::initTableList).start();
@@ -93,10 +75,11 @@ public class AutoGenerateToolWindowContent {
 
     /**
      * 网络走异步
-     * **/
+     **/
     public void initTableList() {
         try {
             Config.initLocalData();
+            addTableName();
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
