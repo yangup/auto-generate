4f897487b33817dc410528306c7e4a0fd6751b01
yangpu <yangpu@ksmglobal.tech>
2024-10-12 10:09:46 +0800

    wrapper

--- src/main/java/com/platform/auto/util/UiUtil.java
+++ src/main/java/com/platform/auto/util/UiUtil.java
@@ -7,15 +7,15 @@ public class UiUtil {
 
     public static JPanel addComponentToPanel(JComponent component, JPanel panel, boolean needCursor) {
         // 添加按钮和其他组件
-        JPanel out = new JPanel(new BorderLayout());
-        out.add(component, BorderLayout.WEST);
+        JPanel wrapper = new JPanel(new BorderLayout());
+        wrapper.add(component, BorderLayout.WEST);
         if (needCursor) {
             component.setCursor(new Cursor(Cursor.HAND_CURSOR));
         }
-        out.setName(component.getName());
-        out.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 3));
-        panel.add(out);
-        return out;
+        wrapper.setName(component.getName());
+        wrapper.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 3));
+        panel.add(wrapper);
+        return wrapper;
     }
 
     public static void setParentBackgroundColor(JComponent childComponent, Color defaultColor) {
