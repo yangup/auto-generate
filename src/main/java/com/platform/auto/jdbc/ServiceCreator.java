bc480bcf6d32aec7bcc82bbe8e2d4b7bf567b80b
yangpu <yangpu@ksmglobal.tech>
2025-04-08 18:26:24 +0800

    service 优化

--- src/main/java/com/platform/auto/jdbc/ServiceCreator.java
+++ src/main/java/com/platform/auto/jdbc/ServiceCreator.java
@@ -65,7 +65,9 @@ public class ServiceCreator extends BaseCreator {
     private void autowiredService() {
         if (table.relateTable != null) {
             for (PageListParam param : table.relateTable) {
-                codeList.add(t + String.format("private @Autowired %sService %sService;\n", param.otherTable.tableNameJava, param.otherTable.tableNameJavaParam));
+                codeList.add(t + "private @Autowired -a-Service -b-Service;\n"
+                        .replaceAll("-a-", param.otherTable.tableNameJava)
+                        .replaceAll("-b-", param.otherTable.tableNameJavaParam));
             }
         }
     }
@@ -77,8 +79,12 @@ public class ServiceCreator extends BaseCreator {
             if (!c.isId && isNotEmpty(c.findData)) {
                 note = "";
             }
-            codeList.add(note + wp + String.format("wrapper.eq(isNotEmpty(queryMap.get(\"%s\")), %sEntity::get%s, queryMap.get(\"%s\"));",
-                    c.columnNameJava, table.tableNameJava, firstToUppercase(c.columnNameJava), c.columnNameJava));
+            codeList.add(note + wp +
+                    "wrapper.eq(isNotEmpty(queryMap.get(\"-a-\")), -b-Entity::get-c-, queryMap.get(\"-a-\"));"
+                            .replaceAll("-a-", c.columnNameJava)
+                            .replaceAll("-b-", table.tableNameJava)
+                            .replaceAll("-c-", firstToUppercase(c.columnNameJava))
+            );
         }
     }
 
@@ -88,19 +94,25 @@ public class ServiceCreator extends BaseCreator {
                 if (columnInfo.otherTable == null) {
                     continue;
                 }
-                codeList.add(t2 + String.format("Set<String> %sIds = page.stream().map(d -> d.%s).filter(StringUtils::isNotEmpty).collect(Collectors.toSet());",
-                        columnInfo.otherTable.tableNameJavaParam, columnInfo.columnNameJava));
-                codeList.add(t2 + String.format("PageList<%sData> %sPageList = isNotEmpty(%sIds) ? %sService.find(QueryMap.ofIDS(%sIds).rawTrue()) : null;",
-                        columnInfo.otherTable.tableNameJava, columnInfo.otherTable.tableNameJavaParam, columnInfo.otherTable.tableNameJavaParam,
-                        columnInfo.otherTable.tableNameJavaParam, columnInfo.otherTable.tableNameJavaParam));
+                codeList.add(t2 + "Set<String> -a-Ids = page.stream().map(d -> d.-b-).filter(StringUtils::isNotEmpty).collect(Collectors.toSet());"
+                        .replaceAll("-a-", columnInfo.otherTable.tableNameJavaParam)
+                        .replaceAll("-b-", columnInfo.columnNameJava)
+                );
+                codeList.add(t2 + "PageList<-b-Data> -a-PageList = isNotEmpty(-a-Ids) ? -a-Service.find(QueryMap.ofIDS(-a-Ids).rawTrue()) : null;"
+                        .replaceAll("-a-", columnInfo.otherTable.tableNameJavaParam)
+                        .replaceAll("-b-", columnInfo.otherTable.tableNameJava)
+                );
             }
             for (PageListParam param : table.relateTable) {
-                codeList.add(t2 + String.format("Set<String> %ss = page.stream().map(d -> d.%s).filter(StringUtils::isNotEmpty).collect(Collectors.toSet());",
-                        param.thisTableColumn.columnNameJava, param.thisTableColumn.columnNameJava));
-                codeList.add(t2 + String.format("PageList<%sData> %sPageList = isNotEmpty(%ss) ? %sService.find(QueryMap.of(\"%s\", %ss).rawTrue()) : null;",
-                        param.otherTable.tableNameJava, param.otherTable.tableNameJavaParam, param.thisTableColumn.columnNameJava,
-                        param.otherTable.tableNameJavaParam,
-                        param.otherTableColumn.columnNameJava, param.thisTableColumn.columnNameJava));
+                codeList.add(t2 + "Set<String> -a-s = page.stream().map(d -> d.-a-).filter(StringUtils::isNotEmpty).collect(Collectors.toSet());"
+                        .replaceAll("-a-", param.thisTableColumn.columnNameJava)
+                );
+                codeList.add(t2 + "PageList<-a-Data> -b-PageList = isNotEmpty(-c-s) ? -b-Service.find(QueryMap.of(\"-d-\", -c-s).rawTrue()) : null;"
+                        .replaceAll("-a-", param.otherTable.tableNameJava)
+                        .replaceAll("-b-", param.otherTable.tableNameJavaParam)
+                        .replaceAll("-c-", param.thisTableColumn.columnNameJava)
+                        .replaceAll("-d-", param.otherTableColumn.columnNameJava)
+                );
             }
             // 拼接查询
             codeList.add(t2 + String.format("for (%sData a : page) {", table.tableNameJava));
@@ -109,20 +121,25 @@ public class ServiceCreator extends BaseCreator {
                     continue;
                 }
                 // a.customer = customerPageList == null ? null : customerPageList.stream().filter(b -> equals(a.customerId, b.id)).findFirst().orElse(null);
-                codeList.add(t3 + String.format("a.%s = %sPageList == null ? null ? %sPageList.stream().filter(b -> equals(a.%s, b.id)).findFirst().orElse(null);",
-                        columnInfo.otherTable.tableNameJavaParam, columnInfo.otherTable.tableNameJavaParam,
-                        columnInfo.otherTable.tableNameJavaParam, columnInfo.otherTable.tableNameJavaParam));
+                codeList.add(t3 + "a.-a- = -a-PageList == null ? null ? -a-PageList.stream().filter(b -> equals(a.-a-, b.id)).findFirst().orElse(null);"
+                        .replaceAll("-a-", columnInfo.otherTable.tableNameJavaParam)
+                );
             }
             for (PageListParam param : table.relateTable) {
                 if (param.more) {
                     codeList.add(t3 + String.format("a.%sList = %sPageList == null ? null : %sPageList.stream().filter(b -> equals(a.%s, b.%s)).collect(Collectors.toList());",
                             param.otherTable.tableNameJavaParam, param.otherTable.tableNameJavaParam,
                             param.otherTable.tableNameJavaParam, param.otherTable.tableNameJavaParam, param.otherTableColumn.columnNameJava));
+                    codeList.add(t3 + "a.-a-List = -a-PageList == null ? null : -a-PageList.stream().filter(b -> equals(a.-a-, b.-b-)).collect(Collectors.toList());"
+                            .replaceAll("-a-", param.otherTable.tableNameJavaParam)
+                            .replaceAll("-b-", param.otherTableColumn.columnNameJava)
+                    );
                 } else {
-                    codeList.add(t3 + String.format("a.%s = %sPageList == null ? null : %sPageList.stream().filter(b -> equals(a.%s, b.%s)).findFirst().orElse(null);",
-                            param.otherTable.tableNameJavaParam, param.otherTable.tableNameJavaParam,
-                            param.otherTable.tableNameJavaParam,
-                            param.thisTableColumn.columnNameJava, param.otherTableColumn.columnNameJava));
+                    codeList.add(t3 + "a.-a- = -a-PageList == null ? null : -a-PageList.stream().filter(b -> equals(a.-b-, b.-c-)).findFirst().orElse(null);"
+                            .replaceAll("-a-", param.otherTable.tableNameJavaParam)
+                            .replaceAll("-b-", param.thisTableColumn.columnNameJava)
+                            .replaceAll("-c-", param.otherTableColumn.columnNameJava)
+                    );
                 }
             }
             codeList.add(t2 + "}");
